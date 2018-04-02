09ae8b3eefecb974d2ed157ba444bfe69fe3dc3f
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 15:29:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash[:spring].values << supply
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
           puts supply
       end
   end
-  
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
